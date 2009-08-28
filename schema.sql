70f78b42666574db3ab5940a80fe6d60085ee9af
nick@sullivanflock.com <nick@macbook-air.local>
2009-08-27 18:40:51 -0700

    Phase 1

--- schema.sql
+++ schema.sql
@@ -1,7 +1,21 @@
--- Note you will need a dump of the AdEngine tables too:
---  mysqldump -u wikicities -pxxxxxxx -h db4 --database wikicities --tables ad_provider ad_slot ad_slot_override ad_provider_value > adengine.mysql
-CREATE DATABASE IF NOT EXISTS athena;
-USE athena;
+CREATE DATABASE IF NOT EXISTS liftium;
+USE liftium;
+
+CREATE TABLE publisher (
+	pub_id int unsigned not null auto_increment,
+	contact_name varchar(255),
+	contact_email varchar(255),
+	website varchar(255),
+) ENGINE=InnoDB;
+
+CREATE TABLE publisher_network_options (
+	pub_id int unsigned not null,
+	network_id smallint unsigned not null,
+	option_keyname varchar(25) NOT NULL,
+	option_value varchar(255) NOT NULL,
+	KEY (pub_id),
+	UNIQUE KEY (network_id, option_keyname)
+) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS network (
 	network_id smallint unsigned not null auto_increment,
@@ -9,15 +23,11 @@ CREATE TABLE IF NOT EXISTS network (
 	notes text,
 	enabled enum('Yes', 'No') default 'Yes',
 	supports_threshold enum('Yes', 'No') default 'No',
-	pay_type enum('Per Click', 'Per Impression') default 'Per Impression',
+	pay_type enum('Per Click', 'Per Impression', 'Per Acquisition') default 'Per Impression',
 	guaranteed_fill enum('Yes', 'No') default 'No',
-	webui_login_url varchar(255),	
-	webui_username varchar(255),	
-	webui_password varchar(255),	
 	PRIMARY KEY (network_id),
 	UNIQUE KEY (network_name)
 ) ENGINE=InnoDB;
-
 
 CREATE TABLE IF NOT EXISTS target_network_linking (
 	network_id smallint unsigned,
@@ -47,7 +57,8 @@ CREATE TABLE IF NOT EXISTS target_value (
 ) ENGINE=InnoDB;
 	
 CREATE TABLE IF NOT EXISTS tag (
-	tag_id smallint unsigned not null auto_increment,
+	tag_id int unsigned not null auto_increment,
+	pub_id int unsigned not null,
 	tag_name varchar(255),
 	notes text,
 	enabled enum('Yes', 'No') default 'Yes',
@@ -57,27 +68,25 @@ CREATE TABLE IF NOT EXISTS tag (
 	auto_update_ecpm enum('Yes', 'No') default 'No',
 	reported_date datetime,
 	reported_ecpm float unsigned, 
-	estimated_cpm float unsigned,
-	threshold float unsigned, 
+	value float unsigned, 
 	tier tinyint unsigned not null default '0',
 	freq_cap tinyint unsigned default null,
 	rej_cap tinyint unsigned default null,
 	rej_time smallint unsigned default null,
 	tag text, -- mutually exclusive with network_id
 	PRIMARY KEY (tag_id),
+	KEY (pub_id),
 	KEY (tag_name),
 	KEY (network_id)
 ) ENGINE=InnoDB;
 
--- Different networks have different tags
 CREATE TABLE IF NOT EXISTS tag_option (
-	tag_id smallint unsigned not null,
+	tag_id int unsigned not null,
 	option_name varchar(25) NOT NULL,
 	option_value varchar(255),
 	PRIMARY KEY (tag_id,option_name(25)),
 	KEY (option_name)
 ) ENGINE=InnoDB;
-
 
 CREATE TABLE IF NOT EXISTS tag_slot_linking (
 	tag_id smallint unsigned not null,
@@ -86,33 +95,12 @@ CREATE TABLE IF NOT EXISTS tag_slot_linking (
 	KEY (as_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS config_archive (
-	config_id int unsigned not null auto_increment,
-	config_date datetime,
-	targeting_profile varchar(255),
-	config text,
-	PRIMARY KEY (config_id)
-) ENGINE=InnoDB;
-
-
 CREATE TABLE IF NOT EXISTS fills_minute (
 	tag_id smallint unsigned not null,
 	minute datetime,
 	attempts int unsigned default 0,
 	loads int unsigned default 0,
 	rejects int unsigned default 0,
-	load_pl_0 int default 0,
-	load_pl_1 int default 0,
-	load_pl_2 int default 0,
-	load_pl_3 int default 0,
-	load_pl_4 int default 0,
-	load_pl_5 int default 0,
-	reject_pl_0 int default 0,
-	reject_pl_1 int default 0,
-	reject_pl_2 int default 0,
-	reject_pl_3 int default 0,
-	reject_pl_4 int default 0,
-	reject_pl_5 int default 0,
 	PRIMARY KEY (tag_id, minute),
 	KEY (minute)
 ) ENGINE=InnoDB;
@@ -123,18 +111,6 @@ CREATE TABLE IF NOT EXISTS fills_hour (
 	attempts int unsigned default 0,
 	loads int unsigned default 0,
 	rejects int unsigned default 0,
-	load_pl_0 int default 0,
-	load_pl_1 int default 0,
-	load_pl_2 int default 0,
-	load_pl_3 int default 0,
-	load_pl_4 int default 0,
-	load_pl_5 int default 0,
-	reject_pl_0 int default 0,
-	reject_pl_1 int default 0,
-	reject_pl_2 int default 0,
-	reject_pl_3 int default 0,
-	reject_pl_4 int default 0,
-	reject_pl_5 int default 0,
 	PRIMARY KEY (tag_id, hour),
 	KEY (hour)
 ) ENGINE=InnoDB;
@@ -145,22 +121,6 @@ CREATE TABLE IF NOT EXISTS fills_day (
 	attempts int unsigned default 0,
 	loads int unsigned default 0,
 	rejects int unsigned default 0,
-	load_pl_0 int default 0,
-	load_pl_1 int default 0,
-	load_pl_2 int default 0,
-	load_pl_3 int default 0,
-	load_pl_4 int default 0,
-	load_pl_5 int default 0,
-	reject_pl_0 int default 0,
-	reject_pl_1 int default 0,
-	reject_pl_2 int default 0,
-	reject_pl_3 int default 0,
-	reject_pl_4 int default 0,
-	reject_pl_5 int default 0,
 	PRIMARY KEY (tag_id, day),
 	KEY (day)
 ) ENGINE=InnoDB;
-
-CREATE VIEW IF NOT EXISTS big_movers_minute (time_slot, tag_id, fill_rate, attempts) AS SELECT IF(minute>(NOW()-INTERVAL 5 MINUTE),'5m', IF(minute>(NOW()-INTERVAL 10 MINUTE),'10m', IF(minute>(NOW()-INTERVAL 15 MINUTE),'15m', IF(minute>(NOW()-INTERVAL 30 MINUTE),'30m','other')))) AS time_slot, tag_id, SUM(loads)/SUM(attempts) AS fill_rate, SUM(attempts) AS attempts FROM fills_minute WHERE minute>(NOW()-INTERVAL 30 MINUTE) GROUP BY tag_id, time_slot;
-CREATE VIEW IF NOT EXISTS big_movers_hour (time_slot, tag_id, fill_rate, attempts) AS SELECT IF(hour>=(NOW()-INTERVAL 2 HOUR),'1h', IF(hour>=(NOW()-INTERVAL 3 HOUR),'2h','other')) AS time_slot, tag_id, SUM(loads)/SUM(attempts) AS fill_rate, SUM(attempts) AS attempts FROM fills_hour WHERE hour>(NOW()-INTERVAL 3 HOUR) GROUP BY tag_id, time_slot;
-CREATE VIEW IF NOT EXISTS big_movers_day (time_slot, tag_id, fill_rate, attempts) AS SELECT IF(day>=(NOW()-INTERVAL 1 DAY),'1d', IF(day>=(NOW()-INTERVAL 2 DAY),'2d','other')) AS time_slot, tag_id, SUM(loads)/SUM(attempts) AS fill_rate, SUM(attempts) AS attempts FROM fills_day WHERE day>(NOW()-INTERVAL 2 DAY) GROUP BY tag_id, time_slot;
