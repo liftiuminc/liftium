ee7335264028d27c948b8f7ebf9469d7a58d6e5b
Nick Sullivan <nick@liftium.com>
2009-09-05 00:15:28 -0700

    Update to proper environment settings

--- dashboard/config/environment.rb
+++ dashboard/config/environment.rb
@@ -39,5 +39,4 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   #
-   ExceptionNotifier.exception_recipients = %w(nick@liftium.com jennie@liftium.com)
 end
--- dashboard/config/environments/production.rb
+++ dashboard/config/environments/production.rb
@@ -8,6 +8,8 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
+ExceptionNotifier.exception_recipients = %w(nick@liftium.com jennie@liftium.com)
+
 
 # See everything in the log (default is :info)
 # config.log_level = :debug
