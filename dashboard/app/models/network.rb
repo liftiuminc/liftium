79e73adc890d40636f1bfc2dde89dfda536961d7
nick@sullivanflock.com <nick@macbook-air.local>
2009-09-04 22:26:13 -0700

    Make Pay type a drop down.

--- dashboard/app/models/network.rb
+++ dashboard/app/models/network.rb
@@ -2,7 +2,7 @@ class Network < ActiveRecord::Base
   @paytypes = Array["Per Click", "Per Impression", "Affliate" ]
   has_many :tags
   validates_uniqueness_of :network_name
-  validates_presence_of :network_name
+  validates_presence_of :network_name, :pay_type
   validates_inclusion_of :enabled, :in => [true, false]
   validates_inclusion_of :always_fill, :in => [true, false]
   validates_inclusion_of :supports_threshold, :in => [true, false]
