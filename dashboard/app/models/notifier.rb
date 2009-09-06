2e03a5810b42b16dbc64bf4b9db6757d76e41ef9
Nick Sullivan <nick@liftium.com>
2009-09-05 23:50:20 -0700

    Update to use dashboard.liftium.com, slight change in wording

--- dashboard/app/models/notifier.rb
+++ dashboard/app/models/notifier.rb
@@ -1,9 +1,9 @@
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = "www.liftium.com"
+  default_url_options[:host] = "dashboard.liftium.com"
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions from Liftium.com"
-    from          "Liftium Password Reset <noreply@liftium.com>"
+    from          "noreply@liftium.com"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
