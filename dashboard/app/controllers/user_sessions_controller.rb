91fb371be455a25494f9c3e6e311bf65c93c83a1
nick@sullivanflock.com <nick@macbook-air.local>
2009-09-05 21:04:59 -0700

    Get rid of login in favor of e-mail

--- dashboard/app/controllers/user_sessions_controller.rb
+++ dashboard/app/controllers/user_sessions_controller.rb
@@ -10,7 +10,7 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Login successful!"
+      flash[:notice] = "Login successful"
       redirect_back_or_default account_url
     else
       render :action => :new
@@ -19,7 +19,7 @@ class UserSessionsController < ApplicationController
   
   def destroy
     current_user_session.destroy
-    flash[:notice] = "Logout successful!"
+    flash[:notice] = "Logout successful"
     redirect_back_or_default new_user_session_url
   end
 end
