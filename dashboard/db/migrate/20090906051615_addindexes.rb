7771d8d073632a58274deae14cf0a977969900f5
nick@sullivanflock.com <nick@macbook-air.local>
2009-09-05 22:28:14 -0700

    Add migration for proper indexes

---
+++ dashboard/db/migrate/20090906051615_addindexes.rb
@@ -0,0 +1,16 @@
+class Addindexes < ActiveRecord::Migration
+  def self.up
+    add_index(:networks, :network_name, :unique => true);
+    add_index(:adformats, :format_name, :unique => true);
+    add_index(:publishers, :publisher_name, :unique => true);
+    remove_index(:tags, :publisher_id);
+    add_index(:tags, [:publisher_id, :tag_name], :unique => true);
+    add_index(:users, :email, :unique => true);
+  end
+
+  def self.down
+    remove_index(:networks, :network_name);
+    remove_index(:adformats, :format_name);
+    remove_index(:publishers, :publisher_name);
+  end
+end
