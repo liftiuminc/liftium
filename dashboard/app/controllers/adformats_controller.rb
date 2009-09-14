578dce18eb12715a01a1313c2af24a085b5d471f
Nick Sullivan <nick@liftium.com>
2009-09-13 21:53:47 -0700

    Change order of height/width

--- dashboard/app/controllers/adformats_controller.rb
+++ dashboard/app/controllers/adformats_controller.rb
@@ -1,4 +1,7 @@
 class AdformatsController < ApplicationController
   before_filter :require_user
   active_scaffold :adformat
+  active_scaffold :adformat do |config|
+    config.columns = [:format_name, :width, :height ]
+  end
 end
