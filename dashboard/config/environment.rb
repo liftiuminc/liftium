830a2897f4bc9955c41dfc56371acac579e4d6ac
root <root@testubuntu.liftium.com>
2009-09-17 16:58:05 -0700

    utf8

--- dashboard/config/environment.rb
+++ dashboard/config/environment.rb
@@ -3,6 +3,10 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
+# Unicode. 
+$KCODE = 'u'
+require 'jcode'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
