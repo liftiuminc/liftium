2d105cf4db4f675b2ea7e4ce37fb34fe04bde56e
nick <nick@salt.liftium.com>
2009-09-22 17:51:30 -0400

    js packer

---
+++ delivery/js/jspack.php
@@ -0,0 +1,34 @@
+<?php
+// you can pass this script to PHP CLI to minify/obfuscate your javascript
+
+if (empty($argv[1])) {
+        echo 'you must specify  a source file and a result filename',"\n";
+        echo 'example :', "\n", 'php jspack.php myScript-src.js myPackedScript.js',"\n";
+        return;
+} else if (empty($argv[2])){
+	// One file specified. pack in place
+        $out = $argv[1];
+	$tempfile = tempnam("/home/tempfiles/3hours/", basename($out));
+        $src = $tempfile;
+	copy($out, $src);
+} else {
+	$src = $argv[1];
+	$out = $argv[2];
+}
+
+require '../../includes/JavaScriptPacker.php';
+
+$script = file_get_contents($src);
+
+$t1 = microtime(true);
+
+$packer = new JavaScriptPacker($script, 'Normal', true, false);
+$packed = $packer->pack();
+
+$t2 = microtime(true);
+$time = sprintf('%.4f', ($t2 - $t1) );
+echo 'script ', $src, ' packed in ' , $out, ', in ', $time, ' s.', "\n";
+
+file_put_contents($out, $packed);
+?>
+
