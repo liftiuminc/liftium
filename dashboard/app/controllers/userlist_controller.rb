932b2f902f85ec864dd0a9eb126c62a7020a4367
Nick Sullivan <nick@liftium.com>
2009-09-26 01:20:18 -0700

    Allow new accounts to be created

--- dashboard/app/controllers/userlist_controller.rb
+++ dashboard/app/controllers/userlist_controller.rb
@@ -3,9 +3,20 @@ class UserlistController < ApplicationController
 
   active_scaffold :user
   active_scaffold :user do |config|
-    config.columns = [:email, :publisher, :admin, :current_login_at, :last_login_at ]
-    config.create.columns = [:email, :admin, :publisher ]
+    config.columns = [:email, :publisher, :admin, :current_login_at, :current_login_ip, :last_login_at, :last_login_ip ]
+    config.create.columns = [:email, :password, :password_confirmation, :admin, :publisher ]
     config.update.columns = [:email, :admin, :publisher ]
     config.columns[:admin].form_ui = :checkbox
   end
 end
+
+
+# Not working for some reason
+#module UserlistHelper
+#   def password_column(record)
+#      password_field "record", "password", :class => "email-input text-input", :size => 30
+#   end
+#   def password_confirmation_column(record)
+#      password_field "record", "password_confirmation", :class => "email-input text-input", :size => 30
+#   end
+#end
