191ec50529b5739e98ee9e3954d43b8f497ece2d
Nick Sullivan <nick@liftium.com>
2009-09-26 23:43:00 -0700

    Size as a dropdown. Move helper to correct spot

--- dashboard/app/controllers/networks_controller.rb
+++ dashboard/app/controllers/networks_controller.rb
@@ -20,11 +20,3 @@ class NetworksController < ApplicationController
 
   end
 end
-
-module NetworksHelper
-  # Pull this list from Networks.
-  def pay_type_form_column (record, input_name)
-     select("", input_name, ["Per Impression", "Per Click", "Affliate"], { :selected => record.pay_type, :include_blank => true })
-  end
-end
-
--- dashboard/app/helpers/networks_helper.rb
+++ dashboard/app/helpers/networks_helper.rb
@@ -1,2 +1,6 @@
 module NetworksHelper
+  # Pull this list from Networks.
+  def pay_type_form_column (record, input_name)
+     select("", input_name, ["Per Impression", "Per Click", "Affliate"], { :selected => record.pay_type, :include_blank => true })
+  end
 end
