3c1d48ae8db5099c2c54e1a1fbedb64667e9d311
Nick Sullivan <nick@liftium.com>
2009-10-03 20:42:58 -0700

    Now passing with validations and proper fixtures

--- dashboard2/app/models/ad_format.rb
+++ dashboard2/app/models/ad_format.rb
@@ -1,4 +1,12 @@
 class AdFormat < ActiveRecord::Base
   attr_accessible :ad_format_name, :size
+  validates_presence_of :ad_format_name, :size
+  validates_uniqueness_of :ad_format_name
+  validates_format_of :size, :with => /^[0-9]{1,3}x[0-9]{1,3}$/
+
+    def name_with_size
+      "#{format_name} (#{size})"
+    end
+
   belongs_to :tag
 end
