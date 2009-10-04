60e4d9843aa3d113f9d247f5f8d0248823f52ca2
Nick Sullivan <nick@liftium.com>
2009-10-04 01:36:56 -0700

    Network form Usability ++

--- dashboard2/app/models/network.rb
+++ dashboard2/app/models/network.rb
@@ -1,13 +1,34 @@
 class Network < ActiveRecord::Base
   attr_accessible :network_name, :website, :pay_type, :enabled, :supports_threshold, :default_always_fill, :us_only, :comments, :contact_info, :billing_info, :brand_safety_level, :tag_template, :scraping_instructions
 
-  @pay_types = ["Per Click", "Per Impression", "Affliate" ]
+  @all_pay_types = ["Per Click", "Per Impression", "Affliate" ]
   has_many :network_options
   validates_uniqueness_of :network_name
-#  validates_presence_of :network_name, :pay_type
-#  validates_inclusion_of :enabled, :in => [true, false]
-#  validates_inclusion_of :default_always_fill, :in => [true, false]
-#  validates_inclusion_of :supports_threshold, :in => [true, false]
-#  validates_inclusion_of :pay_type, :in => @pay_types, :message => "must be one of: " + @pay_types.join(', ')
+  validates_presence_of :network_name, :pay_type
+  validates_inclusion_of :enabled, :in => [true, false]
+  validates_inclusion_of :default_always_fill, :in => [true, false]
+  validates_inclusion_of :supports_threshold, :in => [true, false]
+  validates_inclusion_of :pay_type, :in => @all_pay_types, :message => "must be one of: " + @all_pay_types.join(', ')
+
+   def enabled_s 
+      enabled ? "Yes" : "No"
+   end
+
+   def supports_threshold_s 
+      supports_threshold ? "Yes" : "No"
+   end
+
+   def default_always_fill_s 
+      default_always_fill ? "Yes" : "No"
+   end
+
+   def us_only_s 
+      us_only ? "Yes" : "No"
+   end
+
+   def pay_types 
+        ["Per Click", "Per Impression", "Affliate" ]
+   end
+  
 
 end
--- dashboard2/spec/models/network_spec.rb
+++ dashboard2/spec/models/network_spec.rb
@@ -1,6 +1,8 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Network do
+  fixtures :networks
+
   it "should be valid" do
     Network.new.should be_valid
   end
