dd38241ef7e23b95a3307fa8258bd07a2a430632
Nick Sullivan <nick@liftium.com>
2009-10-03 20:11:10 -0700

    nifty_generators (unpacked so that we can modify template) rspec

--- dashboard2/config/environment.rb
+++ dashboard2/config/environment.rb
@@ -19,10 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "nifty-generators", :lib => false
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+ 
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
