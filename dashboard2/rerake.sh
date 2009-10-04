1d8ed61ee0e4a20a8a6ecce528fce6382585f99f
Nick Sullivan <nick@liftium.com>
2009-10-04 16:08:24 -0700

    need rerake in this version of the dashboard too :)

---
+++ dashboard2/rerake.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$1" == "" ] ; then
+  env="development"
+else
+  env=$1
+fi
+
+if [ "$1" == "live" ] ; then
+  echo "No more reraking the live website. You owe Nick a margarita for putting this check here, you would have just dropped the live database."
+fi
+
+rake db:drop RAILS_ENV=$env
+rake db:create RAILS_ENV=$env
+rake db:migrate RAILS_ENV=$env
+rake spec:db:fixtures:load RAILS_ENV=$env
