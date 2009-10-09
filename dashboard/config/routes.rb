85a73ffb1a7a5067a07c0b0c2648f7d280462232
Nick Sullivan <nick@liftium.com>
2009-10-08 19:11:33 -0700

    FIx url in left hand nav

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -9,7 +9,7 @@ ActionController::Routing::Routes.draw do |map|
   # FIXME: Is there way to not have to list all these?
   map.select_network 'tags/select_network', :controller => 'tags', :action => 'select_network'
   map.tag_generator  'tags/generator/:id', :controller => 'tags', :action => 'generator'
-  map.tag_generator  'tags/html_preview', :controller => 'tags', :action => 'html_preview'
+  map.tag_html_preview  'tags/html_preview', :controller => 'tags', :action => 'html_preview'
   map.resources :tags, :has_many => [ :ad_formats, :tag_options ]
   map.resource :user_session
   map.resources :users 
