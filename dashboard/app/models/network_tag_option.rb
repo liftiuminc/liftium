703cabc1f9e0c8ad4cbb36dab2614740e587a1bc
Nick Sullivan <nick@liftium.com>
2009-10-11 20:30:21 -0700

    fix network tag options upon initial create

--- dashboard/app/models/network_tag_option.rb
+++ dashboard/app/models/network_tag_option.rb
@@ -4,7 +4,7 @@ class NetworkTagOption < ActiveRecord::Base
   # FIXME make sure that it is a valid network
   validates_format_of :option_name, :with => /^[A-Z_a-z0-9]+$/, :message => "can only contain alphanumeric characters (no spaces)"
   validates_uniqueness_of :option_name, :scope => :network_id
-  validates_presence_of :option_name, :network_id
+  validates_presence_of :option_name
   validates_inclusion_of :required, :in => [true, false]
 
   def required_s 
