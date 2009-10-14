2db773b28d6fbffcceb8e903ca9fc3feb933d933
Nick Sullivan <nick@liftium.com>
2009-10-13 17:29:46 -0700

    Move the helper to the right one

--- dashboard/app/helpers/tag_options_helper.rb
+++ dashboard/app/helpers/tag_options_helper.rb
@@ -1,14 +1,2 @@
 module TagOptionsHelper
-
-  # http://code.google.com/p/google-code-prettify/
-  def pretty_tag(html)
-	if html.blank? 
-	   return ""
-	end
-	code = stylesheet_link_tag "prettify"
-	code += javascript_include_tag "prettify"
-	# Note that prettyprint and lang-html are special classes for the prettifier, 'code' is ours, in liftium.css
-        code += '<pre class="prettyprint lang-html code">' + "\n" + h(html) + "</pre>\n"
-	code += '<script>prettyPrint()</script>'
-  end
 end
