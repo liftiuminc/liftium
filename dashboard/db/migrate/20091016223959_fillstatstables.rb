5c5060008a6583579f04deb89541b247afe46324
Nick Sullivan <nick@liftium.com>
2009-10-16 19:59:05 -0700

    update data type to match tags

--- dashboard/db/migrate/20091016223959_fillstatstables.rb
+++ dashboard/db/migrate/20091016223959_fillstatstables.rb
@@ -1,8 +1,8 @@
 class Fillstatstables < ActiveRecord::Migration
   def self.up
-    execute "CREATE TABLE IF NOT EXISTS fills_minute (tag_id smallint unsigned not null, minute datetime, attempts int unsigned default 0, loads int unsigned default 0, rejects int unsigned default 0, PRIMARY KEY (tag_id, minute))"
-    execute "CREATE TABLE IF NOT EXISTS fills_hour (tag_id smallint unsigned not null, hour datetime, attempts int unsigned default 0, loads int unsigned default 0, rejects int unsigned default 0, PRIMARY KEY (tag_id, hour))"
-    execute "CREATE TABLE IF NOT EXISTS fills_day (tag_id smallint unsigned not null, day date, attempts int unsigned default 0, loads int unsigned default 0, rejects int unsigned default 0, PRIMARY KEY (tag_id, day))"
+    execute "CREATE TABLE IF NOT EXISTS fills_minute (tag_id int(11) NOT NULL, minute datetime, attempts int unsigned default 0, loads int unsigned default 0, rejects int unsigned default 0, PRIMARY KEY (tag_id, minute))"
+    execute "CREATE TABLE IF NOT EXISTS fills_hour (tag_id int(11) NOT NULL, hour datetime, attempts int unsigned default 0, loads int unsigned default 0, rejects int unsigned default 0, PRIMARY KEY (tag_id, hour))"
+    execute "CREATE TABLE IF NOT EXISTS fills_day (tag_id int(11) NOT NULL, day date, attempts int unsigned default 0, loads int unsigned default 0, rejects int unsigned default 0, PRIMARY KEY (tag_id, day))"
   end
 
   def self.down
