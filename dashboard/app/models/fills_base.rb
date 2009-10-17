1653e28eb034445b8abf67f4f35bb17c47cec3ee
Nick Sullivan <nick@liftium.com>
2009-10-17 12:27:43 -0700

    Add all the fancy date drop downs that Veronica wanted

--- dashboard/app/models/fills_base.rb
+++ dashboard/app/models/fills_base.rb
@@ -45,21 +45,9 @@ class FillsBase < ActiveRecord::Base
        query.push('%' + params[:name_search] + '%')
     end
 
-    # Date / time
-    case params[:date_select].to_s.downcase
-      when "today"
-        params[:start_date] = DateTime.now.strftime('%Y-%m-%d 00:00:00')
-      when "yesterday"
-        params[:start_date] = 1.day.ago.strftime('%Y-%m-%d 00:00:00')
-        params[:end_date] = DateTime.now.strftime('%Y-%m-%d 00:00:00')
-      when "last 7 days"
-        params[:start_date] = 7.days.ago.strftime('%Y-%m-%d 00:00:00')
-        params[:end_date] = DateTime.now.strftime('%Y-%m-%d 00:00:00')
-      when "this month"
-        params[:start_date] = DateTime.now.strftime('%Y-%m-01 00:00:00')
-        params[:end_date] = DateTime.now.strftime('%Y-%m-%d 00:00:00')
-    end
-    
+    dates = self.get_date_range(params[:date_select])
+    params[:start_date] = dates[0].nil? ? "" : dates[0]
+    params[:end_date] = dates[1].nil? ? "" : dates[1]
 
     if (! params[:start_date].blank?)
        query[0] += " AND " + col + " >= ? "
@@ -98,4 +86,60 @@ class FillsBase < ActiveRecord::Base
     model.find_by_sql self.search_sql(model, params)
   end 
 
+  # FIXME: Move somewhere else so that other models can use
+  def get_date_range(timeframe)
+   now = DateTime.now
+   dates = [nil, nil]
+
+   # FIXME: Am I doing this the hard way? Shouldn't this be built into rails?
+   case timeframe.to_s.downcase
+      when "today"
+	dates[0] = now.strftime('%Y-%m-%d 00:00:00')
+      when "yesterday"
+        dates[0] = 1.day.ago.strftime('%Y-%m-%d 00:00:00')
+        dates[1] = now.strftime('%Y-%m-%d 00:00:00')
+      when "last 7 days"  || "last week"
+        dates[0] = 7.days.ago.strftime('%Y-%m-%d 00:00:00')
+        dates[1] = now.strftime('%Y-%m-%d 00:00:00')
+      when "this month" || "month to date"
+        dates[0] = now.strftime('%Y-%m-01 00:00:00')
+        dates[1] = now.strftime('%Y-%m-%d 00:00:00')
+      when "last 30 days"
+        dates[0] = 30.days.ago.strftime('%Y-%m-%d 00:00:00')
+        dates[1] = now.strftime('%Y-%m-%d 00:00:00')
+      when "this quarter" || "quarter to date"
+        month = now.strftime("%m").to_i
+        if month.modulo(3) == 1 # jan, apr, jul, oct
+             dates[0] = now.strftime('%Y-%m-01 00:00:00')
+        elsif month.modulo(3) == 2 # feb, may, aug, nov
+             dates[0] = (now - 1.month).strftime('%Y-%m-01 00:00:00')
+        elsif month.modulo(3) == 0 # mar, jun, sep, nov
+             dates[0] = (now - 2.month).strftime('%Y-%m-01 00:00:00')
+        end
+        dates[1] = now.strftime('%Y-%m-%d 00:00:00')
+
+      when "last quarter" 
+        month = now.strftime("%m").to_i
+        if month.modulo(3) == 1 # jan, apr, jul, oct
+             dates[0] = (now - 3.month).strftime('%Y-%m-01 00:00:00')
+             dates[1] = now.strftime('%Y-%m-01 00:00:00')
+        elsif month.modulo(3) == 2 # feb, may, aug, nov
+             dates[0] = (now - 4.month).strftime('%Y-%m-01 00:00:00')
+             dates[1] = (now - 1.month).strftime('%Y-%m-01 00:00:00')
+        elsif month.modulo(3) == 0 # mar, jun, sep, nov
+             dates[0] = (now - 5.month).strftime('%Y-%m-01 00:00:00')
+             dates[1] = (now - 2.month).strftime('%Y-%m-01 00:00:00')
+        end
+
+      when "this year" || "year to date"
+        dates[0] = now.strftime('%Y-01-01 00:00:00')
+        dates[1] = now.strftime('%Y-%m-%d 00:00:00')
+      when "last year" 
+        dates[0] = 1.year.ago.strftime('%Y-01-01 00:00:00')
+        dates[1] = now.strftime('%Y-01-01 00:00:00')
+      when "all time"
+	# no op
+    end
+    return dates
+  end
 end
