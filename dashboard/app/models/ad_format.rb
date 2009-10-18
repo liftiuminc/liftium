a318f657b86fa69bd534690a71492dd082915edc
Nick Sullivan <nick@liftium.com>
2009-10-18 15:58:16 -0700

    starting to clean up tests

--- dashboard/app/models/ad_format.rb
+++ dashboard/app/models/ad_format.rb
@@ -1,5 +1,4 @@
 class AdFormat < ActiveRecord::Base
-  attr_accessible :ad_format_name, :size
   validates_presence_of :ad_format_name, :size
   validates_uniqueness_of :ad_format_name
   validates_format_of :size, :with => /^[0-9]{1,3}x[0-9]{1,3}$/
@@ -8,5 +7,4 @@ class AdFormat < ActiveRecord::Base
     "#{ad_format_name} (#{size})"
   end
 
-  belongs_to :tag
 end
--- dashboard/test/unit/ad_format_test.rb
+++ dashboard/test/unit/ad_format_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class AdFormatTest < ActiveSupport::TestCase
-  should "be valid" do
-    assert AdFormat.new.valid?
-  end
+  should_allow_values_for :size, "728x90", "160x600"
+  should_validate_presence_of :ad_format_name, :size
 end
