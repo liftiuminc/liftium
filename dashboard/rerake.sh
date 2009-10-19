2e20e50c5c0de862875ac1ed9614950290661f44
Jos Boumans <jos@liftium.com>
2009-10-19 03:41:56 -0700

    * leave a comment in rerake.sh explaining why fixtures_dir doesnt work   as expected

--- dashboard/rerake.sh
+++ dashboard/rerake.sh
@@ -12,4 +12,12 @@ fi
 rake db:drop RAILS_ENV=$env
 rake db:create RAILS_ENV=$env
 rake db:migrate RAILS_ENV=$env
+
+### loading fixtures does not appear to work if they are not in the spec/ dir
+### tried with FIXTURES_DIR to an absolute path, relative path and ., no luck
+### despite what the usage says. strace'd the processes to be sure. Switched to
+### a symlink to get it running again. -Jos
+# $ rake -D spec:db:fixtures:load
+#     Load fixtures (from spec/fixtures) into the current environment's database.  Load specific fixtures using FIXTURES=x,y. Load from subdirectory in test/fixtures using FIXTURES_DIR=z.
+#rake spec:db:fixtures:load FIXTURES_DIR=. RAILS_ENV=$env
 rake spec:db:fixtures:load RAILS_ENV=$env
