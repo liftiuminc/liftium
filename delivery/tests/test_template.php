b3e78113c05d5e4733306e8519fbcbb9063588ee
Nick Sullivan <nick@liftium.com>
2009-10-25 12:37:43 -0700

    Add test template

---
+++ delivery/tests/test_template.php
@@ -0,0 +1,14 @@
+<?php require 'header.php'?>
+This page is for testing ____________
+<p>
+<script>
+var somethingGood = true;
+if(somethingGood) {
+	LiftiumTest.testPassed();
+} else {
+	LiftiumTest.testFailed();
+}
+</script>
+
+<?php require 'footer.php'?>
+
