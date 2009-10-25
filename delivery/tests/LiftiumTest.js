005026cc361968700e5f77e7403f7efd2897d516
Nick Sullivan <nick@liftium.com>
2009-10-25 12:47:10 -0700

    Make the test work if the test_results div hasn't been created, and if Liftium javascript object does not exist. Also issue :retab! to convert all spaces to tabs

--- delivery/tests/LiftiumTest.js
+++ delivery/tests/LiftiumTest.js
@@ -4,41 +4,45 @@
 window.failTestOnError = true;
 
 var LiftiumTest = {
-        testsRun : 0,
-        testsPassed : 0,
-        testsFailed : 0,
+	testsRun : 0,
+	testsPassed : 0,
+	testsFailed : 0,
 	errors : []
 };
 
 LiftiumTest.testPassed = function(){
-        if (this.testsFailed === 0){
-                document.body.bgColor = "green";
-        }
-        this.testsRun++;
-        this.testsPassed++;
-        this.updateTestStatus();
+	if (this.testsFailed === 0){
+		document.body.bgColor = "green";
+	}
+	this.testsRun++;
+	this.testsPassed++;
+	this.updateTestStatus();
 };
 
 LiftiumTest.testFailed = function(){
-        document.body.bgColor = "red";
-        this.testsRun++;
-        this.testsFailed++;
-        this.updateTestStatus();
+	document.body.bgColor = "red";
+	this.testsRun++;
+	this.testsFailed++;
+	this.updateTestStatus();
 };
 
 LiftiumTest.updateTestStatus = function(){
-        window.Liftium._("test_results").innerHTML = this.testsRun + " tests ran, " +
+	var test_results = document.getElementById("test_results");
+	if (test_results === null) {
+		document.write('<div id="test_results"></div>');
+	}
+	test_results.innerHTML = this.testsRun + " tests ran, " +
 		this.testsPassed + " tests passed, " +
 		this.testsFailed + " tests failed";
-        window.Liftium._("test_results").style.display = "block";
+	test_results.style.display = "block";
 };
 
 LiftiumTest.getTagStats = function(tag_id){
-        var text = "Raw Tag Stats: " + window.Liftium.tagStats + "<br />";
-        text += "Stats for tag_id #" + tag_id + ":<br />";
-        text += "&nbsp; - Loads = " + window.Liftium.getTagStat(tag_id, "l") + "<br />";
-        text += "&nbsp; - Rejects = " + window.Liftium.getTagStat(tag_id, "r") + "<br />";
-        text += "&nbsp; - Attempts = " + window.Liftium.getTagStat(tag_id, "a") + "<br />";
-        text += "&nbsp; - Last Rejection = " + window.Liftium.getMinutesSinceReject(tag_id) + " minutes ago<br />";
-        return text;
+	var text = "Raw Tag Stats: " + window.Liftium.tagStats + "<br />";
+	text += "Stats for tag_id #" + tag_id + ":<br />";
+	text += "&nbsp; - Loads = " + window.Liftium.getTagStat(tag_id, "l") + "<br />";
+	text += "&nbsp; - Rejects = " + window.Liftium.getTagStat(tag_id, "r") + "<br />";
+	text += "&nbsp; - Attempts = " + window.Liftium.getTagStat(tag_id, "a") + "<br />";
+	text += "&nbsp; - Last Rejection = " + window.Liftium.getMinutesSinceReject(tag_id) + " minutes ago<br />";
+	return text;
 };
