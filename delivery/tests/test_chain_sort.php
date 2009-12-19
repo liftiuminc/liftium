1da09c94ce397b8c8e85681065819847f4ef14bb
Nick Sullivan <nick@liftium.com>
2009-12-19 17:54:47 -0500

    update test for looking at how chains are sorted repeatedly

--- delivery/tests/test_chain_sort.php
+++ delivery/tests/test_chain_sort.php
@@ -5,22 +5,17 @@ This page is for testing the chain sorting
 var chain = [
 	{tag_id: "one - 1.50", tier: 5, adjusted_value: 1.50, count: 0},
 	{tag_id: "two - 1.25", tier: 5, adjusted_value: 1.25, count: 0},
-	{tag_id: "three - 1.00", tier: 5, adjusted_value: 1.00, count: 0}
-];
-var chain2 = [
-	{tag_id: "one - 1.00", tier: 5, adjusted_value: 1.00, count: 0},
-	{tag_id: "two - 0.90", tier: 5, adjusted_value: 0.90, count: 0},
-	{tag_id: "three - 0.80", tier: 5, adjusted_value: 0.80, count: 0}
+	{tag_id: "three - 1.00", tier: 5, adjusted_value: 1.00, count: 0},
+	{tag_id: "four - 0.90", tier: 5, adjusted_value: 1.00, count: 0},
+	{tag_id: "five - 0.80", tier: 5, adjusted_value: 0.90, count: 0},
+	{tag_id: "six - 0.70", tier: 5, adjusted_value: 0.80, count: 0}
 ];
 for (var i = 0; i < 10000; i++){
 	chain.sort(Liftium.chainSort);
 	chain[0].count++;
 
-	chain2.sort(Liftium.chainSort);
-	chain2[0].count++;
 }
 document.write("Chain one:  <pre>" + Liftium.print_r(chain) + "</pre>");
-document.write("<p>Chain two:  <pre>" + Liftium.print_r(chain2) + "</pre>");
 </script>
 <script>
 var somethingGood = true;
