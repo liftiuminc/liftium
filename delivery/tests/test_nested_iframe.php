8713fcbd4cb2c264170228a1f6862ddaa53b63c1
Nick Sullivan <nick@liftium.com>
2010-04-12 17:29:00 -0400

    add test that duplicates the nested iframe bug (red)

---
+++ delivery/tests/test_nested_iframe.php
@@ -0,0 +1,7 @@
+<?php require 'header.php'?>
+This page is for testing an iframe hop, inside another frame. It calls an existing test, <a href="test_ad_iframe_hop.php">test_ad_iframe_hop</a>.
+<p>
+<iframe width=500 height=500 src="test_ad_iframe_hop.php"></iframe>
+
+<?php require 'footer.php'?>
+
