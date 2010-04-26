fdd02bee9755068afeafa5b3c79c6bf73e87a207
nick@sullivanflock.com <nick@macbook-air.local>
2010-04-26 16:49:40 -0700

    dual mode fix

--- delivery/tag.php
+++ delivery/tag.php
@@ -17,12 +17,18 @@ body {
 }
 </style>
 </head>
-<?php $options = Array('pubid' => '', 'autoInit' => false, 'placement' => @$_GET['placement']) ?>
 <script>
-var LiftiumOptions = <?php echo json_encode($options) ?>;
+if (top != self && document.referrer.toString().indexOf(document.domain) > -1){
+  // In an iframe and the parent window can be referenced
+  var Liftium = top.Liftium;
+  var LiftiumOptions = top.LiftiumOptions;
+} else {
+  <?php $options = Array('pubid' => '', 'autoInit' => false, 'placement' => @$_GET['placement']) ?>
+  var LiftiumOptions = <?php echo json_encode($options) ?>;
+  // FIXME: make this url configurable 
+  document.write("<script src='http://liftium.wikia.com/js/Liftium.js'><\/script>");
+}
 </script>
-<!-- FIXME: make this url configurable -->
-<script src='http://liftium.wikia.com/js/Liftium.js'></script>
 <body>
 <?php
 if ($data['network_name'] == 'DART'){
